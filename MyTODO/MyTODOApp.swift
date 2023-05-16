c8224b8967efe6b873ec01f39337f6e949925c24
Amjad Oudeh <Amjad.oudeh@Soundcloud.com>
2023-05-16 10:00:43 +0200

    Initial Commit

---
+++ MyTODO/MyTODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTODOApp.swift
+//  MyTODO
+//
+//  Created by Amjad Oudeh on 16.05.23.
+//
+
+import SwiftUI
+
+@main
+struct MyTODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
