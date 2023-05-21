642ac01c8e4e9e888379c2c793b11b39f9ad9846
Amjad Oudeh <Amjad.oudeh@Soundcloud.com>
2023-05-21 22:16:26 +0200

    Error message for the valid email in signIn form

--- MyTODO/ViewModels/SignInViewViewModel.swift
+++ MyTODO/ViewModels/SignInViewViewModel.swift
@@ -10,10 +10,16 @@ class SignInViewViewModel: ObservableObject {
     }
     
     func signIn() {
+        errorMessage = ""
         guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
               !password.trimmingCharacters(in: .whitespaces).isEmpty else {
             
             errorMessage = "Please make sure that you filled in all fields"
+            return
+        }
+        
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter a valid email"
             return
         }
         print("called")
